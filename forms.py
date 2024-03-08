54a955b89fe562499cd8293e1e7599c87a8ee2bc
Shelly Sachdev <77107785+Shelly1986@users.noreply.github.com>
2024-03-08 09:45:43 +0100

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo,Length
 
 class RegistrationForm(FlaskForm):
@@ -14,3 +14,10 @@ class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
     password = PasswordField('Password',validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class ForgotForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    
+
+class VerifyForm(FlaskForm):
+    otp = StringField('One Time Password',validators=[DataRequired()])
